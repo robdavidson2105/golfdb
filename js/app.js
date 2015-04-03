9aa15a402b559e2eabe07a05d4934581e58c1ea8
Rob Davidson <robdavidson2105@hotmail.com>
2015-04-03 18:29:45 +0100

    test

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 (function(){
 
 	var app = angular.module('courses', []);
-	var selectedHole = 0
 
 	app.controller('courseController', function($scope){
 		var currentUser = Parse.User.current();
@@ -11,12 +10,12 @@
 		} else {
 			$scope.SignedIn = false;
 		}
+		// Initialise a few variables,arrays, objects
 		this.Details = [];
 		var courseEntries = [];
-		//$scope.maps = {};
-		//$scope.selectedCourse = {};
-		//$scope.test = {showMap: [true,true,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false]};
     	this.courseDatabases = courseEntries;
+		
+		// Connect to Parse and query all the courses in the database
     	var GetCourses = Parse.Object.extend("Course");
     	var getCourses = new Parse.Query(GetCourses);
     	getCourses.limit(1000);
@@ -40,13 +39,14 @@
     	
     	$scope.inputType = 'password';
 
+		// Controller function to add a new course
 		this.addCourse = function(currentCourse) {
 			var courseInfos = new GetCourses();
-			var empty = [];
 			courseInfos.set("Name", currentCourse.name);
 			courseInfos.set("Author", $scope.Username);
 			currentCourse.holes =[];
 			var hole = [];
+			// Setup a new course with some default values
 			for (var i = 0; i < 18; i++) { 
 				hole[i] = {HoleIndex: i, Par: 4, Lat: 0, Long: 0, StrokeIndex: 0};
 				currentCourse.holes[i] = hole[i];
@@ -60,18 +60,23 @@
 			});
 		}
 
-
-
+		// Controller function to update a course
 		this.updateCourse = function(currentCourse){
 			var Course = new Parse.Query(GetCourses);
+			// The objectId is a unique identifier which allows us to select the course
 			Course.equalTo("objectId", currentCourse.id);
 			Course.get(currentCourse.id, {
 				success: function (CourseU) {
 					CourseU.set("Name", currentCourse.name);
 					var hole = [];
 					for (var i = 0; i < 18; i++) { 
-						hole[i] = {HoleIndex: i, Par: currentCourse.holes[i].Par, StrokeIndex: currentCourse.holes[i].StrokeIndex, Lat: currentCourse.holes[i].Lat, Long: currentCourse.holes[i].Long};
-						//hole[i] = {HoleIndex: i, Par: currentCourse.pars[i], StrokeIndex: currentCourse.strokeIndex[i]};
+						hole[i] = {
+							HoleIndex: i, 
+							Par: currentCourse.holes[i].Par, 
+							StrokeIndex: currentCourse.holes[i].StrokeIndex, 
+							Lat: currentCourse.holes[i].Lat, 
+							Long: currentCourse.holes[i].Long
+						};
 						currentCourse.holes[i] = hole[i];
 					};
 					CourseU.set("Holes", currentCourse.holes);
@@ -80,6 +85,7 @@
 			});
 		}
 
+		// Controller function to delete a course from the database
 		this.deleteCourse = function(currentCourse){
 			var Course = new Parse.Query(GetCourses);
 			Course.equalTo("objectId", currentCourse.id);
@@ -94,6 +100,7 @@
 			});
 		}
 
+		// Controller function to create a new user
 		this.signUp = function(details){
 			var user = new Parse.User();
 			user.set("username", details.Username);
@@ -109,7 +116,8 @@
 				}		
 			});
 		}
-
+		
+		// Controller function to sign in
 		this.signIn = function(details){
 			Parse.User.logIn(details.Username, details.Password, {
 				success: function(user) {
@@ -124,47 +132,57 @@
 			})
 		}
 
+		// Controller function to logout
 		this.logOut = function(details){
 			Parse.User.logOut();
 			var currentUser = Parse.User.current();
 			window.location.reload(false); 
 		}
 		
-		
+		// Controller function to display a map for the selected course and the selected hole
 		this.showMap = function(i, currentCourse, lat, lon) {
 			var zoomLevel = 18;
 			var drawPin = true;
 			if (lat == 0 || lon == 0)
 			{
 				// if there's not coordinates saved yet - then let's see if there's any from the previous hole
-				// but only if this is hole 2 or above
+				// first set up some default values
 				lat = 54;
 				lon = -2;
 				zoomLevel = 5;
 				drawPin = false;
 				
+				// Only checking previous holes if this isn't the first hole
 				if (i != 0) {
+					// if the previous hole has non-default values - then lets use them
 					if (currentCourse.holes[i-1].Lat != 0 && currentCourse.holes[i-1].Long != 0) {
 						lat = currentCourse.holes[i-1].Lat;
 						lon = currentCourse.holes[i-1].Long;
+						// and zoom out a bit too
 						zoomLevel = 17;
 					}
 				}	
 			}
-			var myCenter=new google.maps.LatLng(lat,lon);
-
+			
+			var myCenter=new google.maps.LatLng(lat,lon);  //centre the map on the lat, lon
+			
+			// Set the default map properties = centred on the hole, zoomed to the right level, and a satellite view
 			var mapProp = {
 				center: myCenter,
 				zoom: zoomLevel,
 				mapTypeId: google.maps.MapTypeId.SATELLITE
 			};
 
+			// if there is no map defined then we need to create one - but lets avoid creating more than one for the same hole
 			if (currentCourse.maps[i]===undefined) {	
-				console.log("map object" + currentCourse.maps[i]);
+				// Each map needs a unique ref, so use the name googleMap + course object id + hole number
 				currentCourse.maps[i] = new google.maps.Map(document.getElementById("googleMap" + currentCourse.id + i), mapProp);
+				// Drop a marker at the hole location
 				if (drawPin) {
 					drawMarker(currentCourse, i, lat, lon);
 				}
+
+				// Register a listener for a click event - if the map is clicked then we'll put a marker at that location
 				google.maps.event.addListener(currentCourse.maps[i],'click',function(e) {
 					$scope.$apply(function(){
 						currentCourse.holes[i].Lat = Math.round(e.latLng.lat() * 1000000) / 1000000;
@@ -172,7 +190,6 @@
 						drawMarker(currentCourse, i, currentCourse.holes[i].Lat, currentCourse.holes[i].Long);
 					});
 				});
-
 			}
 		}	
 	});
