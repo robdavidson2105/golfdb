22d27f9a9e7da5a3d0343057f562741d0a84c5f8
Rob Davidson <robdavidson2105@hotmail.com>
2015-04-03 11:02:35 +0100

    test

--- js/app.js
+++ js/app.js
@@ -136,14 +136,14 @@
 			//$scope.clicked = false;
 			//selectedHole = i;
 			var myCenter=new google.maps.LatLng(lat,lon);
+			//center: myCenter,
 			var mapProp = {
-			center: myCenter,
 			zoom:5,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 			if ($scope.maps[i]===undefined) {
 				console.log("map object" + $scope.maps[i]);
-				$scope.maps[i] = new google.maps.Map(document.getElementById("googleMap" + i),mapProp);
+				$scope.maps[i] = new google.maps.Map(document.getElementById("googleMap" + i), mapProp);
 
 			}
 		}
